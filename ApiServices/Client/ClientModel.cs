382f516f6f8367e918c2d917d07cf7292461ecae
bentzlec <bentzlec@oregonstate.edu>
2023-03-17 10:32:16 -0700

    Reverted changes to 4 files that was causing Licen sees page issues.

--- ApiServices/Client/ClientModel.cs
+++ ApiServices/Client/ClientModel.cs
@@ -8,13 +8,5 @@ namespace RealEstateCRM.ApiServices.Client
     public class ClientModel
     {
         public int id_agent { get; set; }
-        public int id_client { get; set; }
-        public string first_name { get; set; }
-        public string last_name { get; set; }
-        public string address { get; set; }
-        public string city { get; set; }
-        public string email { get; set; }
-        public string phone_num { get; set; }
-        public int sw_active { get; set; }
     }
 }
